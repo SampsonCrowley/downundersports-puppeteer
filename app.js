232dffede44893206b7b60f1f36815567e0360d8
Sampson Crowley <sampsonsprojects@gmail.com>
2019-04-24 21:14:13 -0600

    add wait for queue to complete

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ var parseUrl = function(url) {
     return url;
 };
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function waitForQueue(page) {
+  return new Promise(async (resolve) => {
+    let queue = true
+    while (queue) {
+      queue = await page.evaluate(() => {
+        try {
+          return window.currentFetchQueue.queue.length
+        } catch {
+          return false
+        }
+      })
+      if(queue) await sleep(1000)
+    }
+
+    return resolve()
+  })
+}
+
 app.get('/render', function(req, res) {
   console.log(req.query.url)
   var urlToRender = parseUrl(req.query.url);
@@ -26,13 +48,20 @@ app.get('/render', function(req, res) {
       try {
 
         const page = await browser.newPage();
-        await page.goto(urlToRender);
-        const buffer = await page.evaluate(() => '<!DOCTYPE HTML>\n' + document.documentElement.outerHTML)
+        await page.goto(urlToRender, { waitUntil: ['networkidle0', 'load', 'domcontentloaded'] });
+
+        await waitForQueue(page)
+
+        const buffer    = await page.evaluate(() => '<!DOCTYPE HTML>\n' + document.documentElement.outerHTML),
+              subWindow = await page.evaluate(() => window.ssrHydrationParams);
+
+        console.log(subWindow)
+
 
         res.setHeader('Content-Disposition', 'attachment;filename="' + urlToRender + '.html"');
         res.setHeader('Content-Type', 'text/html');
 
-        await res.send(buffer)
+        await res.send(buffer.replace(/<body([^>]*)>/, '$&<div id="ssr-hydration-params" style="display:none">' + JSON.stringify(subWindow) + '</div>'))
 
         await browser.close();
       } catch (err) {
